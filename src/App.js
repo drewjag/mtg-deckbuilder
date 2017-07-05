13d7f2e3b5082072fdcdc08a5121eedc63b2529d
Drew Jagdat <andrew.jagdat@achievers.com>
2017-07-05 13:07:22 -0400

    Added instructions on how to use the app

--- src/App.js
+++ src/App.js
@@ -66,10 +66,12 @@ class App extends Component {
   componentDidMount() {
     get('https://api.magicthegathering.io/v1/sets?type=expansion')
     .then(JSON.parse)
-    .then(response => this.setState({
+    // The api typically returns fast enough that you wouldn't see the Loader component.
+    // I added a timeout to allow the Loader to be visible so you know it is there.
+    .then(response => setTimeout(() => this.setState({
       isLoading: false,
       sets: _.reverse(response.sets),
-    }));
+    }), 2000));
   }
 
   setMtgSet(event) {
@@ -77,7 +79,7 @@ class App extends Component {
   }
 
   getSealedCardPool() {
-    // this.setState({ isLoading: true });
+    this.setState({ isLoading: true });
     const { selectedSet } = this.state;
     let { cardPool } = this.state;
     const getBoosterURL = `https://api.magicthegathering.io/v1/sets/${selectedSet}/booster`;
@@ -90,6 +92,9 @@ class App extends Component {
           cardPool = _.concat(cardPool, response.cards);
           this.setState({ cardPool, cardsToDisplay: [cardPool] });
         },
+      )
+      .then(
+        () => this.setState({ isLoading: false }),
       ));
     }
   }
@@ -194,6 +199,27 @@ class App extends Component {
               <Col xs={12} sm={12} md={12} lg={12}>
                 <h1>Sealed Deck Builder</h1>
               </Col>
+              <Col xs={12} sm={8} md={8} lg={8}>
+                <h4>Instructions:</h4>
+                <p>
+                  Here you can build a sealed deck out of a pool of cards to
+                  play Magic: The Gathering. You can read more about the sealed
+                  deck format <a href="http://magic.wizards.com/en/game-info/gameplay/formats/sealed-deck">here</a>.
+                </p>
+                <p>
+                  To begin, select a card set from the drop down menu.
+                  To learn more about sets, visit <a href="http://magic.wizards.com/en/products/card-set-archive">this site</a>.
+                </p>
+                <p>
+                  Next, generate your card pool by clicking Open Booster Packs.
+                  This will generate all of the cards you can use to build a deck.
+                  40 cards are needed to make a deck and you will typically choose
+                  about 23 from the card pool to go into that deck. The rest will be
+                  made up of basic lands.
+                </p>
+                <p>Happy battling!</p>
+                <p>This app is built using the <a href="https://docs.magicthegathering.io/#overview">MTG API</a></p>
+              </Col>
             </Row>
             <Row>
               <Col xs={12} sm={4} md={4} lg={4}>
@@ -215,12 +241,24 @@ class App extends Component {
             <Col xs={12} sm={12} md={12} lg={12}>
               <h1>Sealed Deck Builder</h1>
             </Col>
+            <Col xs={12} sm={8} md={8} lg={8}>
+              <p>
+                Add cards to your deck by clicking on them while viewing your card pool.<br />
+                To view your deck, click the View Deck button.<br />
+                To remove a card, simply click it in the deck view.
+              </p>
+              <p>
+                Remember: 40 cards are needed to make a deck and you will typically choose
+                about 23 from the card pool to go into that deck. The rest will be
+                made up of basic lands.
+              </p>
+            </Col>
           </Row>
           <Row>
-            <Button onClick={this.sortCardPoolByColor}>Show All Cards</Button>
+            <Button onClick={this.sortCardPoolByColor}>Organize &amp; Show Card Pool</Button>
             <DropdownButton
               id="colorFilter"
-              title="Filter Card Pool by Color"
+              title="Filter Card Pool"
               onSelect={this.setFilteredCards}
             >
               <MenuItem eventKey="White">White</MenuItem>
@@ -232,7 +270,7 @@ class App extends Component {
               <MenuItem eventKey="Colorless">Colorless</MenuItem>
               <MenuItem eventKey="Land">Land</MenuItem>
             </DropdownButton>
-            <Button onClick={this.getDeck}>View Deck</Button>
+            <Button className="viewDeckButton" onClick={this.getDeck}>View Deck</Button>
           </Row>
           <Row>
             <div className="cardStack">
